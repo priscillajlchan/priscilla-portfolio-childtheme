e68d14485613507bcc6e5027a8d10efe6b3ce5b8
Priscilla <priscillachan__@hotmail.com>
2013-11-25 11:34:04 -0800

    SOCIAL MEDIA ADDED!!!
    Hard coded in twitter, google plus and linked buttons

--- index.php
+++ index.php
@@ -12,14 +12,24 @@ get_header(); ?>
 
 	<div id="homewrapper">
 
-    <!-- <div id="welcomequote">
+	<div id="socmedicon">
+		
+		<a href="https://twitter.com/priscillajlchan"><img class="twitter" src="http://localhost:8888/dev/portfolio/wp-content/themes/BasicWPTheme-child/assets/twitter.png" alt=""></a>
+
+		<a href="http://www.linkedin.com/in/priscillajlchan"><img class="linkedin" src="http://localhost:8888/dev/portfolio/wp-content/themes/BasicWPTheme-child/assets/linkedin.png" alt=""></a>
+
+		<a href="https://google.com/+PriscillaChan225"><img class="google" src="http://localhost:8888/dev/portfolio/wp-content/themes/BasicWPTheme-child/assets/google.png" alt=""></a>
+
+	</div>
+	
+
+    <div id="welcomequote">
 		"Designers are meant to be loved;
 		<br> not understood"
-	</div> -->
+	</div>
 
-	<ul>
-		<a class="twitter" href="#">Twitter</a>
-	</ul>
+	
+
 	
 	<!-- <section id="content"> -->
 		
--- page-2.php
+++ page-2.php
@@ -23,7 +23,7 @@ get_header(); ?>
          <div id="aboutpris">
          	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed neque lacinia, bibendum nulla ut, rutrum urna. Duis tortor velit, convallis vel semper at, vestibulum eu enim. Donec ac imperdiet ante. Ut cursus eleifend augue, nec auctor purus lacinia sit amet. Cras volutpat metus sed sem pulvinar, non rutrum enim aliquet. Mauris in ligula neque. Cras luctus vitae magna eu aliquam. Morbi fermentum ipsum diam. Nunc semper urna ac faucibus venenatis. Suspendisse vestibulum, ligula vitae tempor aliquam, nisl sem adipiscing odio, et lacinia ipsum ipsum eget leo. Cras egestas pulvinar molestie.
 
-			Curabitur lacinia nunc bibendum nibh hendrerit bibendum. Praesent eu nunc sollicitudin neque malesuada commodo ac at sem. Proin consequat eleifend sem at vulputate. Maecenas vel dolor in arcu placerat porttitor ac eu erat. Mauris eget est eget massa eleifend interdum. Vestibulum rutrum, turpis id facilisis elementum, urna sem viverra orci, vitae facilisis est eros eu neque. Morbi varius ligula quis luctus malesuada.</p>
+			Curabitur lacinia nunc bibendum nibh hendrerit bibendum. Praesent eu nunc sollicitudin neque malesuada commodo ac at sem. Proin consequat eleifend sem at vulputate. Maecenas vel dolor in arcu placerat porttitor ac eu erat. Mauris eget est eget massa eleifend interdum.</p>
 
         </div>
 
